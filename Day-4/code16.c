f7ed7b4bf836eb7089ddb72b41c31adb1fb9f2ba
GLB25110CN131 <vaibhakshi12@gmail.com>
2026-06-04 16:03:13 +0530

    Day-4 assignment is uploaded

---
+++ Day-4/code16.c
@@ -0,0 +1,29 @@
+//armstrong number in a range
+
+#include <stdio.h>
+
+int main() {
+    int start, end, num, temp, digit, sum;
+
+    printf("Enter the start and end of range: ");
+    scanf("%d %d", &start, &end);
+
+    printf("Armstrong numbers between %d and %d are:\n", start, end);
+
+    for (num = start; num <= end; num++) {
+        temp = num;
+        sum = 0;
+
+        while (temp != 0) {
+            digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+
+        if (sum == num) {
+            printf("%d ", num);
+        }
+    }
+
+    return 0;
+}
