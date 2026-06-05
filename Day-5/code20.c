245a80d9bcd0e1c20a888e880d800d360031b85f
GLB25110CN131 <vaibhakshi12@gmail.com>
2026-06-05 13:46:16 +0530

    Day-5 assignment is uploaded

---
+++ Day-5/code20.c
@@ -0,0 +1,30 @@
+//find the largest prime factor of a number
+
+#include <stdio.h>
+
+int main() {
+    long long num;
+    long long largestFactor = 0;
+
+    printf("Enter a number: ");
+    scanf("%lld", &num);
+
+    while (num % 2 == 0) {
+        largestFactor = 2;
+        num /= 2;
+    }
+
+    for (long long i = 3; i * i <= num; i += 2) {
+        while (num % i == 0) {
+            largestFactor = i;
+            num /= i;
+        }
+    }
+
+    if (num > 2)
+        largestFactor = num;
+
+    printf("Largest Prime Factor = %lld\n", largestFactor);
+
+    return 0;
+}
