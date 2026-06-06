9986fa975314e16631b2f4f8af2620efc92ebd91
GLB25110CN131 <vaibhakshi12@gmail.com>
2026-06-06 21:05:51 +0530

    Day-6 assignment is uploaded

---
+++ Day-6/code23.c
@@ -0,0 +1,20 @@
+//write a program to count the number of set bits in a given number
+
+#include <stdio.h>
+
+int main() {
+    int num, count = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        if (num & 1)
+            count++;
+        num = num >> 1;
+    }
+
+    printf("Number of set bits = %d\n", count);
+
+    return 0;
+}
