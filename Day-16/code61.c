3eb9de4c83cdefb3d68697b3b19ec0a1a2e42ecf
GLB25110CN131 <vaibhakshi12@gmail.com>
2026-06-16 22:38:26 +0530

    Fixed mistake in code 61

--- Day-16/code61.c
+++ Day-16/code61.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, sum = 0, total;
+    int n, sum1 = 0, sum2 = 0;
 
-    printf("Enter n: ");
+    printf("Enter size of array: ");
     scanf("%d", &n);
 
-    int arr[n - 1];
+    int arr1[n];
 
-    printf("Enter %d elements:\n", n - 1);
-    for(i = 0; i < n - 1; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
+    printf("Enter elements of first array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr1[i]);
+        sum1 += arr1[i];
     }
 
-    total = n * (n + 1) / 2;
+    int arr2[n - 1];
 
-    printf("Missing Number = %d", total - sum);
+    printf("Enter elements of second array (one element missing): ");
+    for (int i = 0; i < n - 1; i++) {
+        scanf("%d", &arr2[i]);
+        sum2 += arr2[i];
+    }
+
+    printf("The missing number is: %d\n", sum1 - sum2);
 
     return 0;
 }
